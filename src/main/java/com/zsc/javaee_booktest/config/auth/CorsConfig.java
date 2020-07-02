b0493a985c303f93f59bb5d6ef736886648bc7c0
ilove2Q <dengguidong208@qq.com>
2020-07-02 19:58:53 +0800

    缓存....

--- src/main/java/com/zsc/javaee_booktest/config/auth/CorsConfig.java
+++ src/main/java/com/zsc/javaee_booktest/config/auth/CorsConfig.java
@@ -19,20 +19,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @Version 1.0
  **/
 @Configuration
-public class CorsConfig{
-/*    @Override
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8888")
                 .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
                 .allowedHeaders("*")
-                *//*.exposedHeaders("access-control-allow-headers",
+                /*.exposedHeaders("access-control-allow-headers",
                         "access-control-allow-methods",
                         "access-control-allow-origin",
                         "access-control-max-age",
-                        "X-Frame-Options")*//*
+                        "X-Frame-Options")*/
                 .allowCredentials(true).maxAge(3600);
-    }*/
+    }
 
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
